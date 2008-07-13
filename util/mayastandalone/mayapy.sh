be11aa674c0bfaaa7f2fd9f210de2cf8369ef93f
byron <byron@642450df-bb48-0410-819d-d0b8e6721ff8>
2008-07-13 14:10:05 +0000

    Added custom callback entry points in sh and py script for maya standalone operation
    git-svn-id: svn://byronimo.de/mayarv/trunk@20 642450df-bb48-0410-819d-d0b8e6721ff8

--- util/mayastandalone/mayapy.sh
+++ util/mayastandalone/mayapy.sh
@@ -8,10 +8,17 @@ export LD_LIBRARY_PATH=$mayapath/lib:$LD_LIBRARY_PATH
 
 # adjust PYTHONPATH to look for maya includes
 export PYTHONPATH=$mayapath/lib/python$2/site-packages
+
 # the python home appears to be repsonsible for the weird interactive input setting !
 # if python itself starts from the maya home, the pyshell does not support convenience functions
 #export PYTHONHOME=$mayapath
+
 scriptdir=`dirname $0`
+
+# ALLOW CUSTOM CALLBACK
+script=$scriptdir/mayapy_custom.sh
+[ -e $script ] && . $script
+
 # start python and initialize it using the mayastartup script
 python$2 -i $scriptdir/mayapystartup.py
 
--- util/mayastandalone/mayapystartup.py
+++ util/mayastandalone/mayapystartup.py
@@ -1,9 +1,20 @@
 import maya.standalone
 maya.standalone.initialize()
 
+import os
+
+# try to load custom settings
+try: 
+	filepath = os.environ[ "PYMAYASTD_CONFIG" ]
+	execfile( filepath )
+except:
+	print "Set PYMAYASTD_CONFIG to point to python script doing additional setup"
+
+
 # init ipython - needs to be available in your local python installation
 try: 
 	import IPython
 	IPython.Shell.start().mainloop()
 except:
 	print "Warning: Failed to load ipython - please install it for more convenient maya python interaction"
+
