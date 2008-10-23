2554cb3b4d7538994d724a2906e5db17a3a188e8
byron <byron@642450df-bb48-0410-819d-d0b8e6721ff8>
2008-10-23 09:57:44 +0000

    bpython.bat: can now properly initialize maya
    git-svn-id: svn://byronimo.de/mayarv/trunk@299 642450df-bb48-0410-819d-d0b8e6721ff8

--- start/bpython.bat
+++ start/bpython.bat
@@ -1,9 +1,16 @@
 
 REM set the python path to find out package
 REM we do not have maya support in the moment
+REM adjust PYTHONPATH to look for maya includes
+set mayapy=%MAYA_LOCATION%\Python\lib\site-packages
+set mayapydll=%MAYA_LOCATION%\Python\DLLs
+set mayadll=%MAYA_LOCATION%\bin
+
 set basedir=%~dp0
 set pyrepobase=%basedir%\..\py
-set PYTHONPATH=%pyrepobase%;%PYTHONPATH%
+
+set PYTHONPATH=%pyrepobase%;%mayapy%;%PYTHONPATH%
+set PATH=%mayapydll%;%mayadll%;%PATH%
 
 REM execute the tool 
 python %*
