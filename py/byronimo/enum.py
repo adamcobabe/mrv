9ba7142ea8ef695c811104185f99ff7f66179a74
Byron <Byron@642450df-bb48-0410-819d-d0b8e6721ff8>
2008-12-03 15:10:23 +0000

    byronimo.enum: added enumeration class baed on Don Garrets work - its very well documented, looks clear and has a full unit test
    git-svn-id: svn://byronimo.de/mayarv/trunk@396 642450df-bb48-0410-819d-d0b8e6721ff8

---
+++ py/byronimo/enum.py
@@ -0,0 +1,201 @@
+# -*- coding: utf-8 -*-
+"""B{byronimo.enum}
+This module is designed to be the equivalent of the enum type in other
+languages. An enumeration object is created at run time, and contains
+named members that are the enumeration elements.
+
+The enumeration is also a tuple of all of the values in it. You can iterate
+through the values, perform 'in' tests, slicing, etc. It also includes
+functions to lookup specific values by name, or names by value.
+
+You can specify your own element values, or use the create factory method
+to create default Elements. These elements are unique system wide, and are
+ordered based on the order of the elements in the enumeration. They also
+are _repr_'d by the name of the element, which is convenient for testing,
+debugging, and generation text output.
+
+Example Code:
+
+# Using Element values
+Colors = Enumeration.create('red', 'green', 'blue')
+
+# Using explicitly specified values
+Borders = Enumeration.create(('SUNKEN', 1),
+							 ('RAISED', 32),
+							 ('FLAT', 2))
+
+x = Colors.red
+y = Colors.blue
+
+assert x < y
+assert x == Colors('red')
+assert Borders.FLAT == 2:
+assert 1 in Borders
+
+@note: slightly modified by Sebastian Thiel
+@newfield revision: Revision
+@newfield id: SVN Id
+"""
+__author__='Don Garret'
+__contact__='garret at bgb dot cc'
+__version__=1
+__license__='freeware'
+__date__="$Date: 2008-05-29 02:30:46 +0200 (Thu, 29 May 2008) $"
+__revision__="$Revision: 16 $"
+__id__="$Id:$"
+__copyright__='(c) 2003 Don Garret'
+
+class Element(object):
+	"""Internal helper class used to represent an ordered abstract value.
+	   
+	The values have string representations, have strictly defined ordering
+	(inside the set) and are never equal to anything but themselves.
+	
+	They are usually created through the create factory method as values
+	for Enumerations.
+
+	They assume that the enumeration member will be filled in before any
+	comparisons are used. This is done by the Enumeration constructor.
+	"""
+	__slots__ = ( "__name", "__value", "enumeration" )
+	
+	def __init__(self, name, value):
+		self.__name = name
+		self.__value = value
+		self.enumeration = None # Will be filled in later
+		
+	def __repr__(self):
+		return self.__name
+
+	def __cmp__(self, other):
+		"""We override cmp only because we want the ordering of elements
+		in an enumeration to reflect their position in the enumeration.
+		"""
+		if (self.__class__ == other.__class__ and
+			self.enumeration is other.enumeration):
+			# If we are both elements in the same enumeration, compare
+			#	values for ordering
+			return cmp(self.__value, other.__value)
+		else:
+			# Otherwise, fall back to the default
+			return NotImplemented
+
+class Enumeration(tuple):
+	"""This class represents an enumeration. You should not normally create
+	multiple instances of the same enumeration, instead create one with
+	however many references are convenient.
+
+	The enumeration is a tuple of all of the values in it. You can iterate
+	through the values, perform 'in' tests, slicing, etc. It also includes
+	functions to lookup specific values by name, or names by value.
+
+	You can specify your own element values, or use the create factory method
+	to create default Elements. These elements are unique system wide, and are
+	ordered based on the order of the elements in the enumeration. They also
+	are _repr_'d by the name of the element, which is convenient for testing,
+	debugging, and generation text output.
+	"""
+
+	def __new__(self, names, values):
+		"""This method is needed to get the tuple parent class to do the
+		Right Thing(tm).
+		"""
+		return tuple.__new__(self, values)
+
+	def __init__(self, names, values):
+		"""The arguments needed to construct this class are a list of
+		element names (which must be unique strings), and element values
+		(which can be any type of value). If you don't have special needs,
+		then it's recommended that you use Element instances for the values.
+
+		This constructor is normally called through the create factory (which
+		will create Elements for you), but that is not a requirement.
+		"""
+
+		assert len(names) == len(values)
+
+		# We are a tuple of our values, plus more....
+		tuple.__init__(self, values)
+
+		self.__nameMap = {}
+		self.__valueMap = {}
+
+		# Store each enum value as a named attribute
+		self.__slots__ = names
+
+		for i in xrange(len(names)):
+			name = names[i]
+			value = values[i]
+
+			# Tell the elements which enumeration they belong too
+			if type(value) == Element:
+				value.enumeration = self
+			
+			# Prove that all names are unique
+			assert not name in self.__nameMap
+
+			# create mappings from name to value, and vice versa
+			self.__nameMap[name] = value
+			self.__valueMap[value] = name
+
+		# create named attributes on ourself.
+		self.attachValuesTo(self)
+
+		# Force this instance to be read only
+		def noset(name, value): raise "No assignments allowed"
+		self.__setattr__ = noset
+
+	def attachValuesTo(self, other):
+		"""create named values on an object to match our elements.
+
+		This is appropriate for use in a constructor, against a class
+		object, or a module object.
+
+		Cannot be used on a new style class instance after initialization
+		is complete.
+		"""
+		for name, value in self.__nameMap.items():
+			setattr(other, name, value)
+
+	def valueFromName(self, name):
+		"""Look up the enumeration value for a given element name.
+		"""
+		return self.__nameMap[name]
+		
+	def nameFromValue(self, value):
+		"""Look up the name of an enumeration element, given it's value.
+			
+		If there are multiple elements with the same value, you will only
+		get a single matching name back. Which name is undefined.
+		"""
+		return self.__valueMap[value]
+
+	__call__ = valueFromName
+
+
+def create(*elements):
+	"""Factory method for Enumerations. Accepts of list of values that
+	can either be strings or (name, value) tuple pairs. Strings will
+	have an Element created for use as their value.
+
+	Example:  Enumeration.create('fred', 'bob', ('joe', 42))
+	"""
+	names = []
+	values = []
+
+	for element in elements:
+		if type(element) == tuple:
+			assert len(element) == 2
+			
+			names.append(element[0])
+			values.append(element[1])
+
+		elif type(element) in (str, unicode):
+			names.append(element)
+			values.append(Element(element, len(names)))
+				
+		else:
+			raise "Unsupported element type"
+
+	return Enumeration(names, values)
+
---
+++ py/byronimotest/byronimo/enum.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""B{byronimotset.byronimo.enum}
+
+@note: adjusted by Sebastian Thiel
+@newfield revision: Revision
+@newfield id: SVN Id
+"""
+
+__author__='Don Garret'
+__contact__='garret at bgb dot cc'
+__version__=1
+__license__='freeware'
+__date__="$Date: 2008-05-29 02:30:46 +0200 (Thu, 29 May 2008) $"
+__revision__="$Revision: 16 $"
+__id__="$Id:$"
+__copyright__='(c) 2003 Don Garret'
+
+import unittest
+import byronimo.enum as Enumeration
+
+class ElementTestCase(unittest.TestCase):
+	def testElementComparisons(self):
+		"""byronimo.enum: testElementComparisons"""
+		e = Enumeration.create('fred', 'bob', 'joe', 'larry', 'moe')
+		e2 = Enumeration.create('red', 'green', 'blue')
+		e3 = Enumeration.create('fred', 'bob', 'joe', 'larry', 'moe')
+		element = e.fred
+
+		self.failUnless(e.fred == e.fred)
+		self.failUnless(e.fred != e.bob)
+		self.failUnless(e.fred < e.bob)
+		self.failUnless(e.fred <= e.bob)
+		self.failUnless(e.bob > e.fred)
+		self.failUnless(e.bob >= e.bob)
+		
+		self.failUnless(element == e.fred)
+		self.failUnless(element < e.bob)
+
+		self.failUnless(e.fred < e.moe)
+		self.failUnless(e.larry > e.bob)
+		self.failUnless(e.joe < e.moe)
+		
+		self.failUnless(e.fred != e2.red)
+		self.failUnless(e.fred != e3.fred)
+
+		enumList = list(e)
+		enumList.sort()
+
+		for i in xrange(len(e)):
+			self.failUnless(enumList[i] == e[i])
+
+		self.failUnless(e == e)
+		self.failUnless(e != e2)
+		self.failUnless(e != e3)
+		
+		self.failUnless(e.fred != 'fred')
+
+	def testElementRepresentation(self):
+		"""byronimo.enum: testElementRepresentation"""
+		e = Enumeration.create('fred', 'bob')
+
+		self.failUnless('fred' == str(e.fred))
+		self.failUnless('bob' == str(e[1]))
+
+	def testElementToEnumeration(self):
+		"""byronimo.enum: testElementToEnumeration"""
+		e = Enumeration.create('fred', 'bob')
+
+		i = e.fred
+
+		self.failUnless(i.enumeration is e)
+		self.failUnless(e.bob.enumeration is e)
+
+	
+class EnumerateTestCase(unittest.TestCase):
+
+	def testMembers(self):
+		"""byronimo.enum: testMembers"""
+		e = Enumeration.create('George',
+							   'John',
+							   ('Paul', 2),
+							   ('Ringo', 'drummer'))
+
+		e.George
+		e.John
+		e.Paul
+		e.Ringo
+
+		try:
+			e.Fred
+			self.fail("Unkown member was found")
+		except:
+			pass
+
+
+	def testTupleness(self):
+		"""byronimo.enum: testTupleness"""
+		e = Enumeration.create('George',
+							   'John',
+							   ('Paul', 2),
+							   ('Ringo', 'drummer'))
+
+		self.failUnless(len(e) == 4)
+
+		constlist = [e.George, e.John, 2, 'drummer']
+
+		extractlist = []
+		for beatle in e:
+			extractlist.append(beatle)
+
+		self.failUnless(constlist == extractlist)
+		self.failUnless(constlist == list(e))
+
+		self.failUnless(e.George in e)
+		self.failUnless('drummer' in e)
+		self.failIf('Fred' in e)
+
+		self.failUnless(e.George == e[0])
+		self.failUnless(e.John	 == e[1])
+		self.failUnless(e.Paul	 == e[2])
+		self.failUnless(e.Ringo	 == e[3])
+
+		
+	def testMultipleEnums(self):
+		"""byronimo.enum: testMultipleEnums"""
+		e  = Enumeration.create('fred', 'bob')
+		e2 = Enumeration.create('joe', 'bob')
+		
+		e.fred
+		e.bob
+		e2.joe
+		e2.bob
+
+		# elements in different sets should NOT be equal
+		self.failUnless(e.bob != e2.bob)
+
+		try:
+			e.joe
+			self.fail("Value from wrong enum")
+		except:
+			pass
+		
+	def testReadOnly(self):
+		"""byronimo.enum: testReadOnly"""
+		e = Enumeration.create('fred', 'bob')
+
+		try:
+			e.fred = e.bob
+			self.fail("value assignment was allowed")
+		except:
+			pass
+
+		try:
+			e[0] = 1
+			self.fail("index assignment was allowed")
+		except:
+			pass
+
+		try:
+			e.joe = 1
+			self.fail("new value assignment was allowed")
+		except:
+			pass
+
+	def testAttachValuesTo(self):
+		"""byronimo.enum: testAttachValuesTo"""
+		e = Enumeration.create('George', 'John',
+							   ('Paul', 2), ('Ringo', 'drummer'))
+
+		def validateObject(o):
+			self.failUnless(e.George == o.George)
+			self.failUnless(e.John == o.John)
+			self.failUnless(2 == o.Paul)
+			self.failUnless('drummer' == o.Ringo)
+	   
+		class TestOldClassInstance(object):
+			pass
+
+		# We can't attach to a new class instance after it is created.
+		class TestNewClassInstance(object):
+			def __init__(self):
+				e.attachValuesTo(self)
+
+		old = TestOldClassInstance()
+		e.attachValuesTo(old)
+		
+		new = TestNewClassInstance()
+
+		validateObject(old)
+		validateObject(new)
+
+
+	def testNameLookup(self):
+		"""byronimo.enum: testNameLookup"""
+		e = Enumeration.create('George', 'John',
+							   ('Paul', 2), ('Ringo', 'drummer'))
+
+		self.failUnless(e.George == e.valueFromName('George'))
+		self.failUnless('drummer' == e.valueFromName('Ringo'))
+		self.failUnless(e('Paul') == 2)
+
+		try:
+			e.valueFromName('jerry')
+			self.fail("invalid name lookup was allowed")
+		except:
+			pass
+
+	def testValueLookup(self):
+		"""byronimo.enum: testValueLookup"""
+		e = Enumeration.create('George', 'John',
+							   ('Paul', 2), ('Ringo', 'drummer'))
+
+		self.failUnless('George' == e.nameFromValue(e.George))
+		self.failUnless('John' == e.nameFromValue(e.John))
+		self.failUnless('Paul' == e.nameFromValue(2))
+		self.failUnless('Ringo' == e.nameFromValue('drummer'))
+		
+if __name__ == '__main__':
+
+	unittest.main()
+		
