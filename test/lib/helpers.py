09a068a0755361ce5d6a22267e47b185089b37eb
byron <byron@642450df-bb48-0410-819d-d0b8e6721ff8>
2009-07-25 16:34:26 +0000

    Added helpers.py testlibrary
    git-svn-id: svn://byronimo.de/mayarv/trunk@560 642450df-bb48-0410-819d-d0b8e6721ff8

---
+++ test/lib/helpers.py
@@ -0,0 +1,10 @@
+
+import os
+
+def fixturePath( name ):
+	"""@return:
+		path to fixture file with ``name``, you can use a relative path as well, like
+		subfolder/file.ext"""
+	return os.path.abspath( os.path.join( os.path.dirname( __file__ ), "../fixtures/%s" % name ) )
+	
+	
