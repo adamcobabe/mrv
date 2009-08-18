84132679224b7ecbbc8f6aceb7de662cb2b36cb8
Sebastian Thiel <byronimo@gmail.com>
2009-08-18 19:27:53 +0200

    issue:040 createDocs: removed obsolete script as it has been replaced by makefile

--- createDocs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# go to our directory 
-cd `dirname $0`
-
-# whiteshark docs
-export PYTHONPATH=$PYTHONPATH:./ext/pyparsing:./ext/pydot:./ext/networkx:./py
-epydoc --config=./doc/epydoc.cfg --inheritance grouped
