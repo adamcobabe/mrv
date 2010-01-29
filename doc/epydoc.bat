03fe87e416421b888383a4805754df48fdafb190
Martin Freitag <martin-mf-freitag@web.de>
2010-01-29 23:29:47 +0100

    code redundancy removed

--- doc/epydoc.bat
+++ doc/epydoc.bat
@@ -4,30 +4,22 @@ REM set the environment
 REM for now we first try to use normal python and if we can't find it
 REM we go for mayapy instead
 
+set PY=python
 set PYTHONPATH=../..;../ext/pyparsing;../ext/pydot;../ext/networkx;%PYTHONPATH%
-IF "%1" =="" GOTO DEFAULT
-IF /I mayapy == %1 GOTO MAYAPY
+
+IF "%1" =="" GOTO GETPYTHON
+IF /I mayapy == %1 set PY=mayapy&& GOTO GETPYTHON
 GOTO HELP
 
-:DEFAULT
-	set PY="default python"
+:GETPYTHON
 	echo.
-	echo Generate docs using python in your PATH ...
-	python -V
-	IF ERRORLEVEL 1 GOTO MAYAPY
-	echo.
-	python epydoc.py -q -q -o html --config=epydoc.cfg --inheritance grouped
-	IF ERRORLEVEL 1 GOTO EPYERROR
-	GOTO END
+	echo Generate docs using %PY% in your PATH ...
+	%PY% -V
+	IF ERRORLEVEL 1 ( IF %PY%==mayapy (GOTO NOPYTHON) ELSE (set PY=mayapy&& GOTO GETPYTHON) )
 	
-:MAYAPY
-	set PY=mayapy
+:DOJOB
 	echo.
-	echo Generating docs using mayapy in your PATH
-	mayapy -V
-	IF ERRORLEVEL 1 GOTO NOPYTHON
-	echo.
-	mayapy epydoc.py -q -q -o html --config=epydoc.cfg --inheritance grouped
+	%PY% epydoc.py -q -q -o html --config=epydoc.cfg --inheritance grouped
 	IF ERRORLEVEL 1 GOTO EPYERROR
 	GOTO END
 	
@@ -39,13 +31,13 @@ GOTO HELP
 
 :EPYERROR
 	echo.
-	echo ERROR: epydoc may not be installed on %PY%
-	IF %PY%=="default python" echo        use "epydoc.bat mayapy" if epydoc is installed on mayapy 
+	echo ERROR: epydoc may not be installed on %PY:python=default python%
+	IF %PY%==python echo        use "epydoc.bat mayapy" if epydoc is installed on mayapy 
 	GOTO END
 	
 :HELP
 	echo Automatically creates html documentation of mayarv to .\html subfolder.
-	echo If .\html does not exists it will be created.
+	echo If .\html does not exist it will be created.
 	echo.
 	echo usage: epydoc.bat [mayapy]
 	echo.
