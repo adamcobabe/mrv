55b7b7650186db426099755d5cef3de22dc7b8ab
Sebastian Thiel <byronimo@gmail.com>
2010-03-13 16:29:07 +0100

    Added startup performance test - its actually faster than plain maya batch

---
+++ test/maya/performance/test_startup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import unittest
+import time
+import sys
+
+class TestStartupPerformance( unittest.TestCase ):
+	"""@note: this test must run alone"""
+	
+	def test_startup(self):
+		st = time.time()
+		import mayarv.maya.nodes
+		elapsed = time.time() - st
+		
+		# skip if it wasn't started standalone
+		if elapsed < 0.1:
+			return
+		
+		print >> sys.stderr, "Initialized mayarv and maya-standalone (import mayarv.maya.nodes) in %f" % elapsed
+		
