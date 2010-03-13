7d5119ab2901d94e699089034f596c232e310f0e
Sebastian Thiel <byronimo@gmail.com>
2010-03-13 19:50:17 +0100

    imrv: Improved setup, it will now import everything from the nodes module - just for testing in a way

--- bin/ipythonstartup.py
+++ bin/ipythonstartup.py
@@ -1,31 +1,64 @@
 # assure site is properly setup
 import sys
-import site
-# assure all sitelibs are available, important for OSX
-for syspath in sys.path:
-	if syspath.endswith('site-packages'):
-		site.addsitedir(syspath, set(sys.path))
-# END for each syspath
-
-# init maya
-import mayarv.maya
-
 import os
+import site
 
-# try to load custom settings
-if "IMAYARV_CONFIG" in os.environ:
-	filepath = os.environ[ "IMAYARV_CONFIG" ]
-	try:
-		execfile( filepath )
-	except Exception:
-		print "Failed to run configuration script"
-else:
-	print "Set IMAYARV_CONFIG to point to python script doing additional setup"
+#{ Initialization 
+# assure all sitelibs are available, important for OSX
+def setup_syspath():
+	"""Assure additional site-packages get initialized"""
+	for syspath in sys.path:
+		if syspath.endswith('site-packages'):
+			site.addsitedir(syspath, set(sys.path))
+	# END for each syspath
 
-# init ipython - needs to be available in your local python installation
-try: 
+def apply_user_configuration():
+	"""Run optional user scripts"""
+	# try to load custom settings
+	if "IMAYARV_CONFIG" in os.environ:
+		filepath = os.environ[ "IMAYARV_CONFIG" ]
+		try:
+			execfile( filepath )
+		except Exception:
+			print "Failed to run configuration script"
+	else:
+		print "Set IMAYARV_CONFIG to point to python script doing additional setup"
+
+def setup_ipython():
+	"""Perform additional ipython initialization"""
 	import IPython
-	IPython.Shell.start().mainloop()
-except:
-	print "Warning: Failed to load ipython - please install it for more convenient maya python interaction"
+	
+	# init maya
+	import mayarv.maya
+	
+	# make default imports
+	ip = IPython.ipapi.get()
+	ip.ex("from mayarv.maya.nodes import *")
+
+def init_ipython():
+	"""Get the main ipython system up and running"""
+	# init ipython - needs to be available in your local python installation
+	try: 
+		import IPython
+	except Exception, e:
+		raise ImportError("Warning: Failed to load ipython - please install it for more convenient maya python interaction: %s" % str(e))
+	# END exception handling
+	
+	ips = IPython.Shell.start()
+	setup_ipython()
+	apply_user_configuration()
+	ips.mainloop()
+	
+# } END initialization
+
+
+################
+setup_syspath()
+init_ipython()
+################
+
+
+ 
+	
+
 
