21a12bbdf9448b83c63b4ac6ebf1e08a3bcc971d
Sebastian Thiel <byronimo@gmail.com>
2010-03-23 15:22:00 +0100

    mrv.maya.automation converterd to resT

--- maya/automation/__init__.py
+++ maya/automation/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """Intiailize the automation and process framework for maya """
+__docformat__ = "restructuredtext"
+
+#{ Initialization
 
 # assure we only do certain things once
 if 'init_done' not in locals():
